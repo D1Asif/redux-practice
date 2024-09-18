1509beda5a71611215244ca834a1b718d156778c
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-09-18 23:14:13 +0600

    counter implemented using redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import './App.css'
+import { decrement, increment } from './redux/features/counterSlice';
+import { useAppDispatch, useAppSelector } from './redux/hook'
 
 function App() {
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
+  const dotCount = Math.floor(count / 5);
 
   return (
-    <div className='flex justify-center items-center h-screen w-full'>
+    <div className='flex flex-col justify-center items-center h-screen w-full'>
       <div className='flex p-10 bg-slate-50 rounded-md'>
-        <button className='px-3 py-2 bg-green-500 text-white rounded-md font-semibold text-xl'>
+        <button
+          onClick={() => dispatch(increment())}
+          className='px-3 py-2 bg-green-500 text-white rounded-md font-semibold text-xl'
+        >
           Increment
         </button>
-        <h1 className='mx-10 text-3xl'>0</h1>
-        <button className='px-3 py-2 bg-red-500 text-white rounded-md font-semibold text-xl'>
+        <h1 className='mx-10 text-3xl'>
+          {count}
+        </h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className='px-3 py-2 bg-red-500 text-white rounded-md font-semibold text-xl'
+        >
           Decrement
         </button>
       </div>
+      {
+        !!dotCount && (
+          <div className='mt-5 px-10 py-5 bg-slate-50 rounded-md'>
+            {
+              [...Array(dotCount).keys()].map((item) => (
+                <span key={item}>🟢</span>
+              ))
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
